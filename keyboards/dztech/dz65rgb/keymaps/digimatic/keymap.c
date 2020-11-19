3beec405c19c0c43434f93192166722bacbf6b84
Peter Westerström <peter.westerstrom@eonreality.com>
2020-11-19 09:25:03 +0100

    Add numeric layer and rearrange pscr/pause on digimatic dz65rgb

--- keyboards/dztech/dz65rgb/keymaps/digimatic/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/digimatic/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "digimatic.h"
 
-enum { _SV = 0, _SVD, _US, _DV = _US, _FL, _MAC, _FK };
+enum { _SV = 0, _SVD, _US, _DV = _US, _FL, _MAC, _FK, _NL };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS,          KC_DEL,  \
   LT(_FL,KC_NUBS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP, \
          SHIFT_LP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,SHIFT_RP,                   KC_UP,   KC_PGDN, \
-          KC_LCTL, KC_LGUI, KC_LALT,                LT(_FL,KC_SPC),            KC_RALT,             MO(_FL), KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
+          KC_LCTL, KC_LGUI, KC_LALT,                LT(_FL,KC_SPC),            KC_RALT,             MO(_FL), MO(_NL),          KC_LEFT, KC_DOWN, KC_RGHT),
 
 [_SVD] = LAYOUT_65_ansi(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,       KC_BSPC,     KC_INS,  \
@@ -36,7 +36,7 @@ SHIFT_LP,  KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W, 
   KC_LCTL, KC_LALT, KC_LGUI,                   _______,                       KC_RALT, _______, _______,               _______, _______, _______),
 
 [_FL] = LAYOUT_65_ansi(
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,  KC_F12,    _______,      C(KC_INS), \
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,  KC_F12,  C(KC_PSCR),  C(KC_PAUS), \
   TG(_FK), KC_BTN1, KC_BTN2, KC_BTN3, RESET,   _______, _______, _______, TG(_MAC),RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD,BL_TOGG,_______, \
   KC_CAPS, DF(_US), DF(_SV), _______, EEP_RST, _______, _______, _______, KC_MPLY, KC_MSTP, KC_MPRV,  KC_MNXT,         _______,         KC_HOME, \
 SHIFT_LP2, DF(_DV), DF(_SVD),_______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_VOLD, KC_VOLU, KC_MUTE,SHIFT_RP2,                  TOP,     KC_END,  \
@@ -46,9 +46,15 @@ SHIFT_LP2, DF(_DV), DF(_SVD),_______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_VOL
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       _______,     _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______,  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, \
   _______, _______, _______,                   _______,                       _______, _______, _______,               _______, _______, _______),
 
+[_NL] = LAYOUT_65_ansi(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______, \
+  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,          _______, \
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,          _______,           KC_F12, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_VOLU, _______, \
+  _______, _______, _______,                   _______,                       _______, _______, _______,               _______, KC_VOLD, _______),
 };
 // clang-format on
 
--- keyboards/dztech/dz65rgb/keymaps/digimatic/readme.md
+++ keyboards/dztech/dz65rgb/keymaps/digimatic/readme.md
@@ -1,15 +1,18 @@
-# The digimatic keymap for dz65rgb (hotswap)
+# The digimatic keymap for dz65rgb (hotswap) v1
 
-Press FN+R to enter flash mode.
+Press FN+R to enter flash mode. There is also a physical switch on the bottom side of the PCB.
+
+My switches are Kailh Speed Bronze: Clicky | 1.1mm operating | 3.5mm total | 50g Actuation | 65g Bottom Out
+
 
 Build hex:
 ```sh
-make kdztech/dz65rgb:digimatic
+make kdztech/dz65rgb/v1:digimatic
 ```
 
 Build and flash:
 ```sh
-make dztech/dz65rgb:digimatic:dfu-util
+make dztech/dz65rgb/v1:digimatic:dfu-util
 ```
 
 
